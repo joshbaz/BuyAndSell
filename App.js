fbf61f4dd4bf247ff957b51cbb5c87a4405e9184
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-02-12 16:55:20 +0300

    Image

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text,TouchableWithoutFeedback,StatusBar as Bar, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log(require("./assets/splash.png"));
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        React Native
+      </Text>
+      <TouchableWithoutFeedback>
+      <Image 
+      blurRadius={0.4}
+      source={{
+        width:200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}} />
+      </TouchableWithoutFeedback>
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+   
+    paddingTop: Platform.OS === "android" ? Bar.currentHeight : 0,
   },
 });
